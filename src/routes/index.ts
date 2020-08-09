0893689ad6437219f465e33ccc0294e99d9acf04
Egor Zorin <zorinj@gmail.com>
2020-08-09 13:25:25 +0300

    Fix: pre commit

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Request, Response, Express } from 'express';
 export default (app: Express) => {
   app.use((req: Request, res: Response) => {
     res.status(200).json({
-      title: 'Hello from the API',
+      title: 'Hello from the node API',
     });
   });
 };
