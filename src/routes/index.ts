d9a760d94dc64df38a0705b07575f0ecf2c3d646
Egor Zorin <zorinj@gmail.com>
2020-08-09 12:54:28 +0300

    Fix by prettier

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,8 @@ import { Request, Response, Express } from 'express';
 
 export default (app: Express) => {
   app.use((req: Request, res: Response) => {
-    res
-      .status(200)
-      .json({
-        title: 'Hello from the API'
-      })
-  })
-}
+    res.status(200).json({
+      title: 'Hello from the API',
+    });
+  });
+};
