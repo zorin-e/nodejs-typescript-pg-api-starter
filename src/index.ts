e3e026fb65b9c6fcff4c1d581e575ea630c29159
Egor Zorin <zorinj@gmail.com>
2020-12-01 18:10:12 +0300

    Add: env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express from 'express';
 import routes from '@/infrastructure/Routes';
 import morgan from 'morgan';
+import path from 'path';
+import dotenv from 'dotenv';
+
+const nodeEnv = process.env.NODE_ENV;
+const isProd = nodeEnv === 'production';
+const envFileName = isProd ? '.env' : '.env.' + nodeEnv;
+
+dotenv.config({
+  path: path.resolve(__dirname, '../' + envFileName),
+});
 
 const port = process.env.PORT || 5000;
 const app = express();
